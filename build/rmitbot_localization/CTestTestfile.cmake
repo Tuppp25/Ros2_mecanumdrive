930f9f447fbd5ca1c765e1b0518c1bfc9dc5e7fb
vudinhso <vudinhso@gmail.com>
2025-04-18 19:53:19 +0700

    first commit

--- build/rmitbot_localization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vudinhso/RMIT_Bot/lesson5_ws/src/rmitbot_localization
-# Build directory: /home/vudinhso/RMIT_Bot/lesson5_ws/build/rmitbot_localization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
